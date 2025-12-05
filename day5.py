8f461396537986e481bd992b81c1e6ef2cffec86
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2025-12-05 17:06:58 +0100

    Update day5.py

--- day5.py
+++ day5.py
@@ -7,13 +7,11 @@ print("Part 1:", sum(any(int(id) in r for r in fresh_ranges)
 
 fresh_ranges.sort(key=lambda x: x[0])
 merged_ranges = [fresh_ranges[0]]
-merged_index = 0
 for r in fresh_ranges[1:]:
-    if r[0] in (mr := merged_ranges[merged_index]):
-        merged_ranges[merged_index] = range(mr[0], max(mr[-1], r[-1])+1)
+    if r[0] in (mr := merged_ranges[-1]):
+        merged_ranges[-1] = range(mr[0], max(mr[-1], r[-1])+1)
         continue
 
     merged_ranges.append(r)
-    merged_index += 1
 
 print("Part 2:", sum(r[-1] - r[0] + 1 for r in merged_ranges))
