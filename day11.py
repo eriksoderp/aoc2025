e07039d6b768f0ac83cfc58cceeae005a6a06f44
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2025-12-14 13:31:59 +0100

    Update day11.py

--- day11.py
+++ day11.py
@@ -5,7 +5,7 @@ graph = {row[0][:-1]: row[1:] for row in (line.split()
 @cache
 def dfs(src, dest):
     if src == dest: return 1
-    else: return sum(dfs(child, dest) for child in graph[src])
+    return sum(dfs(child, dest) for child in graph[src])
 
 @cache
 def dfs2(src, dest, fft_visited, dac_visited):
@@ -13,7 +13,7 @@ def dfs2(src, dest, fft_visited, dac_visited):
     elif src == 'dac': dac_visited = True
 
     if src == dest: return int(fft_visited and dac_visited)
-    else: return sum(dfs2(child, dest, fft_visited, dac_visited) for child in graph[src])
+    return sum(dfs2(child, dest, fft_visited, dac_visited) for child in graph[src])
 
 print("Part 1:", dfs('you', 'out'))
 print("Part 2:", dfs2('svr', 'out', False, False))
